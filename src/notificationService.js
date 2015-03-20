79e70283455a5be5bc7ce095769176a1713afc51
Grégory Ghez <gghez@users.noreply.github.com>
2015-03-20 20:42:54 +0100

    Create notificationService.js

---
+++ src/notificationService.js
@@ -0,0 +1,41 @@
+(function(){
+    angular.module('angularNotification').service('Notification', [function () {
+    
+        var callbacks = {};
+    
+        function addCallback(channel, cb) {
+            if (!callbacks[channel]) {
+                callbacks[channel] = [];
+            }
+            if (callbacks[channel].indexOf(cb) == -1) {
+                callbacks[channel].push(cb);
+            }
+        }
+    
+        function removeCallback(channel, cb) {
+            if (callbacks[channel]) {
+                var cbidx = callbacks[channel].indexOf(cb);
+                if (cbidx != -1) {
+                    callbacks[channel].splice(cbidx, 1);
+                }
+            }
+        }
+    
+        function notify(channel) {
+            var notifyArgs = Array.prototype.slice.call(arguments, 1);
+    
+            if (callbacks[channel]) {
+                callbacks[channel].forEach(function (cb) {
+                    cb.apply(undefined, notifyArgs);
+                });
+            }
+        }
+    
+        return {
+            register: addCallback,
+            unregister: removeCallback,
+            notify: notify
+        };
+    
+    }]);
+})();
